e2a25872c07f924daedd5b4e7ed5ca62702c6e44
SyedBurhan555 <syedburhanali555@gmail.com>
2023-04-26 00:23:35 +0500

    feat/express-learn-core-function

--- Express/expressPractice.js
+++ Express/expressPractice.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const fs = require('fs');
+const path = require('path')
 const app = express();
 const port = 8000;
 
+app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Syed Burhan Ali shah</h1>")
+app.get('/hello/:name',(req,res)=>{
+    res.send("<h1>Syed Burhan Ali shah</h1>" + req.params.name)
 })
 app.get('/about',(req,res)=>{
     res.send("<h1>this is about page</h1>")
 })
-
+app.get('/website',(req,res)=>{
+    // const web = fs.readFileSync('../index.html')
+    // res.send(web.toString())
+    // res.sendFile(path.join(__dirname,'../index.html'))
+    res.json({'burhan':"ali"})
+})
+app.get('',(req,res)=>{
+    res.send("sorry 404 page not found")
+})
 app.listen(port,()=>{
     console.log(`Server is running on PORT ${port}`)
 })
