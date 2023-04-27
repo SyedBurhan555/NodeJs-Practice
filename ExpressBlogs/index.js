b2e8e3a7b2962b4a48940cfa205a49e5ac6fed13
SyedBurhan555 <syedburhanali555@gmail.com>
2023-04-28 00:17:06 +0500

    feat/handlerbar-practice-continue

--- ExpressBlogs/index.js
+++ ExpressBlogs/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
+const exphbs  = require('express-handlebars');
 const app = express();
 const port = 8000;
 
 
-app.use(express.static(path.join(__dirname,'templates')))
-
+// app.use(express.static(path.join(__dirname,'views')))
 app.use('/',require(path.join(__dirname,'routes/blog.js')))
+
+app.engine('handlebars',exphbs.engine());
+app.set('view engine', 'handlebars');
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
